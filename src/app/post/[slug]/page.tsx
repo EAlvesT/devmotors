45bbec4b3eabc1f2cbf85bf26a1fcf945a2c7eb5
EAlvesT <106121688+EAlvesT@users.noreply.github.com>
2025-07-31 09:25:33 -0300

    first-commit

---
+++ src/app/post/[slug]/page.tsx
@@ -0,0 +1,59 @@
+import { Suspense } from 'react';
+import { getItemBySlug } from '@/utils/actions/get-data';
+import { PostProps } from '@/utils/post.type';
+import { Metadata } from 'next';
+import { Content } from './components/content';
+import { LoadingPost } from './components/loading';
+
+export async function generateMetadata({ params }: {
+    params: { slug: string }
+}): Promise<Metadata>{
+    try{
+        const { slug } = await params;
+        const { objects }: PostProps = await getItemBySlug(slug)
+        .catch(() => {
+            return {
+                title: "DevMotors - Sua oficina especializada!",
+                description: "Oficina de carros em São Paulo"
+            }
+        })
+
+        return{
+            title: `DevMotors - ${objects[0].title}`,
+            description: `${objects[0].metadata.description.text}`,
+            keywords: ["devmotors", "troca de oleo", "devmotors troca de oleo", `${objects[0].title}`],
+            openGraph: {
+                images: [objects[0].metadata.banner.url],
+                title: `DevMotors - ${objects[0].title}`
+            },
+            robots: {
+                index: true,
+                follow: true,
+                nocache: true,
+                googleBot: {
+                    index: true,
+                    follow: true,
+                    noimageindex: true
+                }
+            }
+        }
+
+    }catch(err){
+        return {
+            title: "DevMotors - Sua oficina especializada!",
+            description: "Oficina de carros em São Paulo"
+        }
+    }
+}
+
+export default async function Page({ params }: { params: { slug: string } }){
+    const { slug } = await params;
+
+    return(
+        <>
+            <Suspense fallback={<LoadingPost/>}>
+                <Content slug={slug}/>
+            </Suspense>
+        </>
+    )
+}
---
+++ src/utils/actions/get-data.ts
@@ -0,0 +1,62 @@
+import { redirect } from "next/navigation";
+
+// function delay(ms: number){
+//     return new Promise(resolve => setTimeout(resolve, ms))
+// }
+
+export async function getDataHome() {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects/67a20f7403a8330f06b9eaeb?pretty=
+            true&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata,type`, { next: { revalidate: 120 } })
+
+        if (!res.ok) {
+            throw new Error("Failed to fetch data")
+        }
+
+        return res.json();
+    } catch (err) {
+        throw new Error("Failed to fetch data")
+    }
+}
+
+export async function getSubMenu() {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects?pretty=true&query=
+            %7B%22type%22%3A%22pages%22%7D&limit=10&skip=0&read_key=${process.env.READ_KEY}&depth=1&props=slug%2Ctitle&sort=-order`, { next: { revalidate: 120 } })
+
+        if (!res.ok) {
+            throw new Error("Failed to fetch data")
+        }
+
+        return res.json();
+    } catch (err) {
+        throw new Error("Failed to fetch data")
+    }
+}
+
+export async function getItemBySlug(itemSlug: string) {
+    const baseUrl = `${process.env.NEXT_PUBLIC_API_URL}/objects/`
+
+    //Definindo o objeto de consulta pelo slug
+    const queryParams = new URLSearchParams({
+        query: JSON.stringify({
+            slug: itemSlug
+        }),
+        props: 'slug,title,content,metadata',
+        read_key: process.env.READ_KEY as string
+    })
+
+    const url = `${baseUrl}?${queryParams.toString()}`;
+
+    try {
+        const res = await fetch(url, { next: { revalidate: 120 } })
+
+        if (!res.ok) {
+            throw new Error("Failed to get item by slug")
+        }
+
+        return res.json();
+    } catch (err) {
+        redirect("/")
+    }
+}
